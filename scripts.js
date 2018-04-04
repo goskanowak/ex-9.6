c287dcdbffbb3e25b22fff6e5057e3bf00513ea9
Małgorzata Nowak <goskanowak87@gmail.com>
2018-04-05 00:19:25 +0200

    Add length element

--- scripts.js
+++ scripts.js
@@ -4,18 +4,13 @@ var list = document.getElementById('list');
 
 add.addEventListener('click', function() {
     var element = document.createElement('li');
+        
     element.innerHTML = 'item';
     
-    // lenght
-    // getElementsByTagName oraz właściwości length, aby dowiedzieć się, ile elementów ma lista ma w danym momencie.
-
-
-
-    var number = document.getElementsByTagName('list');
-    
+      var elementLength = document.getElementsByTagName('li');
+        elementLength.length;
+        console.log(elementLength);
+          
     list.appendChild(element);
     }
 );
-
-
-
