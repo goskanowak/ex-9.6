9cf8e3a66d694d9e52ef56e46c61b5698461e4cb
Małgorzata Nowak <goskanowak87@gmail.com>
2018-04-05 14:28:10 +0200

    Add numbering elements

--- scripts.js
+++ scripts.js
@@ -3,14 +3,12 @@ var button = document.getElementById('add');
 var list = document.getElementById('list');
 
 add.addEventListener('click', function() {
-    var element = document.createElement('li');
-        
-    element.innerHTML = 'item';
+    var element = document.createElement('li'),
+        elementsLength = document.getElementsByTagName('li'); 
     
-      var elementLength = document.getElementsByTagName('li');
-        elementLength.length;
-        console.log(elementLength);
+    element.innerHTML = 'item ' + elementsLength.length;
+    
+    console.log(elementsLength);
           
     list.appendChild(element);
-    }
-);
+});
